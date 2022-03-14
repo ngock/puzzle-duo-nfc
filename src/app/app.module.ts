d898c256d8e3ba5774cbefa6383191624d3f7a35
Tran Bich Ngoc <64283002+ngock@users.noreply.github.com>
2022-03-14 16:44:49 +0100

    made pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 const MAT = [
   MatCardModule,
@@ -20,7 +22,13 @@ const MAT = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ...MAT
+    ...MAT,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
