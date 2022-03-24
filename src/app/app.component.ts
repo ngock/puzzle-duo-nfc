eb10a8bbaf27bfaf9c82e7134c6035aae706728e
Tran Bich Ngoc <64283002+ngock@users.noreply.github.com>
2022-03-24 07:39:22 +0100

    updated custom UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,172 +1,45 @@
-import { Component } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { Component, Input, OnInit } from '@angular/core';
+//import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
-  selector: 'app-root',
-  template: `
-    
-    <mat-card *ngIf="!isScanning" class="mat-elevation-z1" [style.backgroundColor]="cards[0].color">
-      <mat-card-header>
-        <mat-card-title>{{ cards[0].title }}</mat-card-title>
-      </mat-card-header>
-      <img mat-card-image src="{{ cards[0].image }}">
-      <mat-card-actions>
-        <button mat-button color="secondary" (click)="start()" >PLAY</button>
-      </mat-card-actions>
-    </mat-card>
-    
-
-    <ul *ngIf="isScanning">
-      <li *ngFor="let card of cards">
-        
-        <mat-card [attr.id]="'card-' + card.id" class="mat-elevation-z1" [style.backgroundColor]="card.color">
-          <mat-card-header>
-            <mat-card-title>{{ card.title }}</mat-card-title>
-          </mat-card-header>
-          <img mat-card-image src="{{ card.image }}">
-          <mat-card-actions>
-            <button mat-button color="primary" (click)="save(card.id)" >SAVE</button>
-            <button mat-button color="secondary" (click)="player.play()" >PLAY</button>
-            <button mat-button color="secondary" (click)="write()">WRITE</button>
-          </mat-card-actions>
-        </mat-card>
-
-        <audio [attr.id]="'audio-'+card.id" src="{{ card.audio }}" #player></audio>
-        
-      </li>
-    </ul>
-  `,
-  styles: [`
-    :host {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding-top: 100px;
-    }
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      display: flex;
-      flex-wrap: wrap;
-    }
-    li {
-      flex: 1;
-      padding: 10px;
-      display: flex;
-      justify-content: center;
-    }
-    mat-card {
-      width: 300px
-    }
-  `]
+  selector: 'app-root', // here muss be updated, not 'lib-soo-nfc-scanner'
+  templateUrl: './app.component.html',
+  styleUrls: []
 })
-export class AppComponent {
-  cards = [ {
-    title: 'Mouse',
-    image: '/assets/images/mouse.jpg',
-    id: 'MOUSE',
-    audio: '/assets/audio/mouse.mp3',
-    color: "#ffffff"
-  }, {
-    title: 'Squirrel',
-    image: '/assets/images/squirrel.jpg',
-    id: 'SQUIRREL',
-    audio: '/assets/audio/squirrel.mp3',
-    color: "#ffffff"
-  }, {
-    title: 'Bee',
-    image: '/assets/images/bee.jpg',
-    id: 'BEE',
-    audio: '/assets/audio/bee.mp3',
-    color: "#ffffff"
-  }, {
-    title: 'Cat',
-    image: '/assets/images/cat.jpg',
-    id: 'CAT',
-    audio: '/assets/audio/cat.mp3',
-    color: "#ffffff"
-  }, {
-    title: 'Rabbit',
-    image: '/assets/images/rabbit.jpg',
-    id: 'RABBIT',
-    audio: '/assets/audio/rabbit.mp3',
-    color: "#ffffff"
-  }, {
-    title: 'Panda',
-    image: '/assets/images/panda.jpg',
-    id: 'PANDA',
-    audio: '/assets/audio/panda.mp3',
-    color: "#ffffff"
-  }];
+export class AppComponent implements OnInit{
+  //@HostBinding('class.soo-plugin-component') sooPluginComponent: Host = true;
 
-  isScanning = false;
+  @Input() text: string = '';
 
-  constructor(private snackBar: MatSnackBar) { }
+  constructor() { }
 
-  async start() {
-    try {
-      const ndef = new (window as any).NDEFReader();
-      this.isScanning = true;
-      await ndef.scan();
-      this.notify("Scanning a tag...");
+  ngOnDestroy(): void {
+    
+  }
 
-      ndef.addEventListener("readingerror", () => {
-        this.notify(`Error reading tag`);
-      });
+  ngOnInit(): void {
+  }
 
-      ndef.addEventListener("reading", ({ message, serialNumber }: { message: any, serialNumber: string }) => {
+  title = 'soo-nfc-scanner';
 
-        for (const record of message.records) {
-          this.notify("Record id:    " + record.id);
-          switch (record.recordType) {
-            case "text":
-              const textDecoder = new TextDecoder(record.encoding);
-              this.play(textDecoder.decode(record.data));
-              this.notify(`Scanned: ${textDecoder.decode(record.data)}`);
-              break;
-            case "url":
-              // TODO: Read URL record with record data.
-              break;
-            default:
-            // TODO: Handle other records with record data.
-          }
-        }
-        this.play('cat');
-      });
-    } catch (error) {
-      this.notify(error + " try it on smartphone with Chrome Android Browser");
-      this.isScanning = false;
+    async start() {
+      try {
+        const ndef = new (window as any).NDEFReader();
+        await ndef.scan();
+        console.log("> Scan started");
+        alert("Scanning a tag...");
+  
+        ndef.addEventListener("readingerror", () => {
+          console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+          alert(`Error reading tag`);
+        });
+  
+        ndef.addEventListener("reading", ({ message, serialNumber }: { message: any, serialNumber: string }) => {
+  
+        
+        });
+      } catch (error) {
+        alert(error + " try it on smartphone with Chrome Android Browser");
+        
+      }
     }
-  }
-
-  notify(message: string) {
-    this.snackBar.open(message, 'OK');
-  }
-
-  async save(id: string) {
-    try {
-      const ndef = new (window as any).NDEFReader();
-      await ndef.write({
-        records: [{ recordType: "text", data: id }]
-      })
-      this.notify(`${id} saved!`);
-    } catch (error) {
-      this.notify(error);
-    }
-  }
-
-  play(id: string) {
-    const player = document.querySelector(`#audio-${id}`) as HTMLAudioElement;
-    const card = document.querySelector(`#card-${id}`) as HTMLElement;
-    card.classList.add('mat-elevation-z8');
-    card?.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
-    player.play();
-    player.onended = () => card.classList.remove('mat-elevation-z8');
-    this.notify(id);
-  }
-
-  write() {
-    console.log("TODO: test : write on NFC Tag");
-  }
 }
